89a07730367387900bc6f92337319067a4273c76
Vinci <24s051007@stu.hit.edu.cn>
2025-03-31 22:43:32 +0800

    update

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -42,9 +42,32 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 
 /// TODO: Finish sys_trace to pass testcases
 /// HINT: You might reimplement it with virtual memory management.
-pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
+use crate::task::read_syscall_count;
+pub fn sys_trace(trace_request: usize, id: usize, data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    match trace_request{
+        0 => {
+            let ptr = id as *const u8;
+            unsafe{
+                return ptr.as_ref().unwrap().clone() as u8 as isize;
+            }
+        },
+        1 => {
+            let ptr = id as *mut u8;
+            let data = data as u8;
+            unsafe{
+                ptr.as_mut().unwrap().clone_from(&data);
+            }
+            0
+        }
+        2 => {
+            let r = read_syscall_count(id);
+            r as isize
+        },
+        _ => {
+            -1
+        }
+    }
 }
 
 // YOUR JOB: Implement mmap.
